c5fe6bda777fcb1ae3def37d62cdc2aa5e4131c4
LWarimaM <warimaly19@gmail.com>
2024-07-22 01:45:43 +0300

    Add files via upload

---
+++ New folder/CAT2_Q3.js
@@ -0,0 +1,9 @@
+<script>
+    var employee name;
+    var hour worked
+    var rate per hour
+rate=hours worked*tax
+window.prompt("enter employee name")
+window.prompt("enter hours the employee has worked")
+window.prompt("enter rate per hour")
+</script>
